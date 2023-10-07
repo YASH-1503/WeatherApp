a7cf1fb57357f3701a8e9f3a52ec25bf34306a6c
yash jain <106433919+YASH-1503@users.noreply.github.com>
2023-10-07 22:35:12 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,3 @@
-// import 'package:flutter/material.dart';
-// // import 'package:weather_app/screens/WeatherScreen.dart';
-// import 'package:weatherapp/screens/WeatherScreen.dart';
-//
-// import 'models/WeatherModel.dart';
-//
-// void main() => runApp(MyApp());
-//
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: WeatherScreen(
-//         weather: WeatherModel(
-//           cityName: 'New York',
-//           temperature: 20.0,
-//           weatherDescription: 'Sunny',
-//         ),
-//       ),
-//     );
-//   }
-// }
 import 'package:flutter/material.dart';
 import 'package:location/location.dart';
 import 'package:http/http.dart' as http;
